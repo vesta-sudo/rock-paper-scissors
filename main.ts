8ab847c4424135f3a41715b672f4365ff903cce4
vesta-sudo <115300048+vesta-sudo@users.noreply.github.com>
2022-10-19 13:56:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Hand = randint(1, 3)
+    if (Hand == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (Hand == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (Hand == 1) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
+let Hand = 0
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    `)
